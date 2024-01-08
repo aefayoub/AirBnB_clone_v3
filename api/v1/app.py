9ee41fef4dffa3ab4b0d01255452e1306fc3e496
= <=>
2024-01-08 13:50:53 -0800

    cmt api directory

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,25 +2,26 @@
 """
 API
 """
-
 import os
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown(self):
     """teardown handling"""
     storage.close()
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     """handles page 404"""
     status = {"error": "Not found"}
     return jsonify(status), 404
+
 
 if __name__ == '__main__':
     try:

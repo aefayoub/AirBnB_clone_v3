00cb32b4d8f04037efce311986ae69f3bf1cdf10
= <=>
2024-01-09 06:48:08 -0800

    cmt changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-API
+API Entry Point
 """
 from api.v1.views import app_views
 from builtins import KeyError
@@ -11,12 +11,14 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+# Define a teardown function to close the database connection
 @app.teardown_appcontext
 def teardown(self):
     """teardown handling"""
     storage.close()
 
 
+# Define an error handler for 404 Not Found errors
 @app.errorhandler(404)
 def page_not_found(error):
     """handles page 404"""
@@ -24,7 +26,9 @@ def page_not_found(error):
     return jsonify(status), 404
 
 
+# Entry point to run the application
 if __name__ == '__main__':
+    # Retrieve the host and port from environment variables.
     try:
         host = os.environ.get('HBNB_API_HOST')
     except KeyError:
@@ -35,4 +39,5 @@ if __name__ == '__main__':
     except KeyError:
         port = '5000'
 
+    # Run the Flask application with the specified host and port
     app.run(host=host, port=port)
